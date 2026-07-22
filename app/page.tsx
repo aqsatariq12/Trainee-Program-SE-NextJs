af1ac456145e690a4b5463ca5e6c83bf7bc03732
aqsatariq12 <aqsatariq2452003@gmail.com>
2026-07-22 10:02:33 +0500

    TASK4: Image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 function Home() {
   return (
-    <h1>Home Page</h1>
-  )
+    <div>
+      <h1>Home Page</h1>
+      <Image src={"/cat.png"} width={500} height={400} alt="Cat" />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
